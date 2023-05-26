a55d13e50953dad636d75e1c2ecbec1e6ddd1c50
AleksSkyr <greshnik1499@gmail.com>
2023-05-26 09:45:55 +0300

    test

--- src/Main.java
+++ src/Main.java
@@ -3,16 +3,11 @@ public class Main {
     public static void main(String[] args) {
         Book AtTheMountainsOfMadness = new Book(" Говарда Лавкрафта", "Хребет безумия.", 1936);
         AtTheMountainsOfMadness.printBook();
-//        System.out.println("Автор: " + AtTheMountainsOfMadness.getAuthorName());
-//        System.out.println("Книга: " + AtTheMountainsOfMadness.getBookName());
         System.out.println("Полное имя автора: " + AtTheMountainsOfMadness.setAuthorName("Говарда Филлипса Лавкрафта"));
         System.out.println("Написание: " + AtTheMountainsOfMadness.setPublishingYear(1931));
-//        System.out.println("Год издания: " + AtTheMountainsOfMadness.getPublishingYear());
         Book NineteenEightyFour = new Book("Джордж Оруэлл", "1984.",1949);
         NineteenEightyFour.printBook();
-//        System.out.println("Автор: " + NineteenEightyFour.getAuthorName());
-//        System.out.println("Книга: " + NineteenEightyFour.getBookName());
-//        System.out.println("Год издания: " + NineteenEightyFour.getPublishingYear());
+        System.out.println("Полное имя автора: " + NineteenEightyFour.setAuthorName("Джордж Оруэлл"));
         NineteenEightyFour.setPublishingYear(1943);
         System.out.println("Придуман в: " + NineteenEightyFour.setPublishingYear(1943));
 
