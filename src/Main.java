37b0a9849790fad6940815aca1b0991fffbe8413
AleksSkyr <greshnik1499@gmail.com>
2023-05-28 12:33:04 +0300

    Book

--- src/Main.java
+++ src/Main.java
@@ -8,10 +8,13 @@ public class Main {
         Book theMountainsOfMadness = new Book( author,"Хребет безумия.", 1936);
         Book nineteenEightyFour = new Book( author1, "1984.",1949);
 
+        theMountainsOfMadness.setPublishingYear(19);
+        nineteenEightyFour.setPublishingYear(1456);
+
         System.out.println("Автор: " + theMountainsOfMadness.getAuthor().foolAuthor() + " Название книги: "
-                + theMountainsOfMadness.getBookName() + " Изданна в: " + theMountainsOfMadness.setPublishingYear(1928));
+                + theMountainsOfMadness.getBookName() + " Изданна в: " + theMountainsOfMadness.getPublishingYear());
         System.out.println("Автор: " + nineteenEightyFour.getAuthor().foolAuthor() + " Название книги: "
-                + nineteenEightyFour.getBookName() + " Изданна в: " + nineteenEightyFour.setPublishingYear(1456));
+                + nineteenEightyFour.getBookName() + " Изданна в: " + nineteenEightyFour.getPublishingYear());
 
         System.out.println("Автор: " + author.foolAuthor());
         System.out.println("Автор: " + author1.foolAuthor());
